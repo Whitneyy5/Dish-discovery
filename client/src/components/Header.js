be0e8bbcb523549489624ff371aad9e0c03ec9f1
Whitney <akehwhitney5@gmail.com>
2024-04-04 14:16:51 +0100

    addition of Header and Routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,32 @@
 import React from "react";
+import dish from "../assets/dish.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <p className=" text-[red]">I am the header</p>
+      <div className="font-consulsans bg-[#663056] font-medium text-[#f8f8ff] h-[2.2rem] pt-1">
+        <h3 className="text-center justify-center ">
+          YOUR CULINARY COMPASS : DISH DISCOVERY
+        </h3>
+      </div>
+      <div className="sectiona fixed z-40 w-full bg-white top-0 flex items-center text-[#001712] px-[7vw] py-2 justify-between">
+        <img src={dish} className="w-[5vw] h-[5vh]" alt="" />
+        <div className="flex gap-6">
+          <Link to={"/"} href="" className="hover:underline">
+            <a>HOME</a>
+          </Link>
+          <Link to={"About"} href="" className="hover:underline">
+            <a>ABOUT</a>
+          </Link>
+          <Link to={"Recipe"} href="" className="hover:underline">
+            <a>RECIPE</a>
+          </Link>
+          <Link to={"Shop"} href="" className="hover:underline">
+            <a>SHOP</a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
