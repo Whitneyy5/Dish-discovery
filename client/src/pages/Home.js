475ff8dac6a6c465274eae49c6c8a9e20caea7e7
Whitney <akehwhitney5@gmail.com>
2024-04-05 23:54:53 +0100

    adding images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import quick from "../assets/quick.jpeg";
-import salad from "../assets/salad.jpeg";
-import health from "../assets/healthy.jpeg";
-import breakfast from "../assets/breakfast.jpeg";
+import QUICK from "../assets/quick and easy.jpg";
+import SALADS from "../assets/saladss.jpg";
+import HEALTHY from "../assets/healthy3.jpg";
+import BREAKFAST from "../assets/breakfast.webp";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <section className="pt-8">
-        <div className="flex flex-wrap items-center gap-4 pt-6 w-[100%]">
-           <img src={quick} alt="" />
-           <img src={salad} alt="" />
-           <img src={health} alt="" />
-           <img src={breakfast} alt="" />
+      <section className="pt-8 px-10">
+        <div className="flex gap-4 h-[25rem] w-[20rem]">
+          <img src={QUICK} alt="" />
+          <img src={SALADS} alt="" />
+          <img src={HEALTHY} alt="" />
+          <img src={BREAKFAST} alt="" />
         </div>
       </section>
       <Footer />
