cf0b8edd11d20bb8ffd1cac999e9ca97ca95405e
Whitney <akehwhitney5@gmail.com>
2024-04-12 13:19:23 +0100

    adding transitions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import dish from "../assets/dish.png";
 import { Link } from "react-router-dom";
 import { IoMdClose } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
 // import { IoIosArrowForward } from "react-icons/io";
 
 const Header = () => {
@@ -37,23 +38,41 @@ const Header = () => {
 
       {/* nav bar section */}
 
-      <div className="sectiona h-[40px] z-40 w-full bg-white pt-3 top-0 flex items-center text-[#001712] px-[7vw] py-2 justify-between">
+      <div className="sectiona h-[40px] z-40 w-full bg-white pt-3 top-0 flex items-center text-[#001712] px-[7vw] py-10 mt-9 justify-between font-bold">
         <img src={dish} className="w-[5vw] h-[7vh]" alt="" />
         <div className="flex gap-6">
-          <Link to={"/"} href="" className="hover:underline">
-            HOME
+          <Link
+            to={"/"}
+            href=""
+            className="hover:underline decoration-[#663056] decoration-2"
+          >
+            <h6>HOME</h6>
           </Link>
-          <Link to={"about"} href="" className="hover:underline">
-            ABOUT
+          <Link
+            to={"about"}
+            href=""
+            className="hover:underline decoration-[#663056] decoration-2"
+          >
+            <h6>ABOUT</h6>
           </Link>
-          <Link to={"recipes"} href="" className="hover:underline">
-            RECIPES
+          <Link
+            to={"recipes"}
+            href=""
+            className="hover:underline decoration-[#663056] decoration-2"
+          >
+            <h6>RECIPES</h6>
           </Link>
-          <Link to={"shop"} href="" className="hover:underline">
-            SHOP
+          <Link
+            to={"shop"}
+            href=""
+            className="hover:underline decoration-[#663056] decoration-2"
+          >
+            <h6>SHOP</h6>
           </Link>
+          <IoSearch className="size-6 text-[#663056]" />
         </div>
       </div>
+      <hr />
       {/* end of nav bar section */}
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,14 +10,43 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <section className="pt-8 px-10">
-        <div className="flex gap-4 h-[25rem] w-[20rem]">
-          <img src={QUICK} alt="" />
-          <img src={SALADS} alt="" />
-          <img src={HEALTHY} alt="" />
-          <img src={BREAKFAST} alt="" />
+      {/* <hr /> */}
+      <section className="bg-[#F7F7F7]">
+        <div className="py-8 overflow-hidden max-w-[69rem] mx-auto ">
+          {/* 4 images start */}
+          <div className=" gap-4 w-full grid grid-cols-4">
+            <div className="relative grid place-items-center hover:opacity-60 transition-all duration-300 cursor-pointer">
+              <img src={QUICK} className="h-[25rem] object-cover " alt="" />
+              <h3 className=" absolute -bottom-3.5 border-0 bg-[#edb654] text-white py-2 w-fit px-6 text-[0.7rem] tracking-[0.15em] font-semibold">
+                QUICK AND EASY
+              </h3>
+            </div>
+
+            <div className="relative grid place-items-center hover:opacity-60 transition-all duration-300 cursor-pointer">
+              <img src={SALADS} className="h-[25rem] object-cover " alt="" />
+              <h3 className=" absolute -bottom-3.5 border-0 bg-[#edb654] text-white py-2 w-fit px-6 text-[0.7rem] tracking-[0.15em] font-semibold">
+                SALADS
+              </h3>
+            </div>
+
+            <div className="relative grid place-items-center hover:opacity-60 transition-all duration-300 cursor-pointer">
+              <img src={HEALTHY} className="h-[25rem] object-cover " alt="" />
+              <h3 className=" absolute -bottom-3.5 border-0 bg-[#edb654] text-white py-2 w-fit px-6 text-[0.7rem] tracking-[0.15em] font-semibold">
+                HEALTHY
+              </h3>
+            </div>
+
+            <div className="relative grid place-items-center hover:opacity-60 transition-all duration-300 cursor-pointer">
+              <img src={BREAKFAST} className="h-[25rem] object-cover " alt="" />
+              <h3 className=" absolute -bottom-3.5 border-0 bg-[#edb654] text-white py-2 w-fit px-6 text-[0.7rem] tracking-[0.15em] font-semibold">
+                BREAKFAST
+              </h3>
+            </div>
+          </div>
+          {/* end of 4 images */}
         </div>
       </section>
+
       <Footer />
     </div>
   );
