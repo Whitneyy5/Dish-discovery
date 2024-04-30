a5fded0169e52fd9de1e06118cbbb047c073ebbd
Whitney <akehwhitney5@gmail.com>
2024-04-30 14:06:40 +0100

    adding about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,7 +6,189 @@ const About = () => {
   return (
     <div>
       <Header />
+      <section>
+        {/* who we are div */}
+        <div>
+          <div>
+            <h2>Who We Are</h2>
+          </div>
 
+          <div>
+            <h3>
+              Dish Discovery is here to help you cook delicious meals with less
+              stress and more joy. We offer recipes and cooking advice for home
+              cooks, by home cooks. Helping create “kitchen wins” is what we are
+              all about. Dish Discovery was founded in 2020 by Akeh Whitney as a
+              home cooking blog to record her favorite family recipes. Today,
+              Dish Discovery has grown into a trusted resource for home cooks
+              with more than 3,000 tested recipes, guides, and meal plans,
+              drawing over 15 million readers each month from around the world.
+              We are supported by a diverse group of recipe developers, food
+              writers, recipe and product testers, photographers, and other
+              creative professionals. Most importantly, Dish Discovery connects
+              home cooks with their greatest sources of inspiration — other home
+              cooks. We're the world's leading digital food brand, and that
+              inspires us to do everything possible to keep our community
+              connected. Sixty-million home cooks deserve no less.
+            </h3>
+          </div>
+        </div>
+        {/* end of who we are div */}
+
+        {/* history div */}
+        <div>
+          <div>
+            <h2>Our History</h2>
+          </div>
+
+          <div>
+            <div>
+              <h3>
+                Founded in 2020, Dish Discovery changed the food world by
+                providing the tools to share recipes and cooking tips, while
+                celebrating the expertise of home cooks online. Since then, Dish
+                Discovery has become the world's largest community-driven food
+                brand, providing trusted resources to more than 60 million home
+                cooks each month.
+              </h3>
+            </div>
+            <div>
+              <h3>
+                Every day, cooks from around the world publish recipes and
+                inspire one another through recipe photos, ratings, reviews, and
+                videos. The combination of the Dish Discovery community with our
+                team of editorial and kitchen professionals provides authority
+                found nowhere else on the internet and has turned the brand into
+                an indispensable resource for cooks of all skill levels.
+              </h3>
+            </div>
+            <div>
+              <h3>
+                You can connect with us and our 11M followers on social media,
+                too. On TikTok, learn how you should store avocados or find out
+                if air fryer coconut shrimp lives up to the hype. On Facebook,
+                Pinterest, and X, browse easy weeknight meals and scour reviews
+                of the world's best air fryers. Find photo-worthy dinner
+                inspiration on Instagram. And on YouTube, cook along with Chef
+                John, who serves up creative, new recipes weekly — alongside a
+                few good laughs thanks to his well-timed puns.
+              </h3>
+            </div>
+            <div>
+              <h3>
+                If you own an Amazon Alexa or Google Assistant device, you'll
+                find our recipes easily discoverable via Voice search.
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        {/* end of history div */}
+
+        {/* community div begins */}
+        <div>
+          <div>
+            <h2>Dish Discovery Community</h2>
+          </div>
+
+          <div>
+            <h3>
+              The heart of Dish Discover is our community of home cooks who
+              share their beloved family recipes, create new recipes, and
+              photograph, rate, and review each other's recipes. Each week, more
+              than 15 million registered members add more than 2,000 recipe
+              ratings, 800 new recipe photos, and almost 200 new recipes to the
+              site. And every minute of every day, 27 people are saving recipes
+              they love or want to try later.
+            </h3>
+          </div>
+        </div>
+
+        {/* community div ends */}
+
+        {/* diversity div begins */}
+
+        <div>
+          <div>
+            <h2>Diversity & Inclusion</h2>
+          </div>
+
+          <div>
+            <div>
+              <h3>
+                We are committed to the goal of having contributors, featured
+                cooks, featured recipes, and stories reflect the diversity of
+                the Dish Discovery community in our digital properties and in
+                the magazine and know that we still have much work to do.
+              </h3>
+            </div>
+
+            <div>
+              <h3>
+                We are working to highlight more of the stories and traditions
+                of our diverse audience. In 2024, we are reviewing and editing
+                content representing 20 percent of our traffic, with the goal of
+                removing any bias in language and instances of cultural
+                appropriation, including language around race, gender, sexual
+                orientation, and glamorized colonialism.
+              </h3>
+            </div>
+
+            <div>
+              <h3>
+                We are also focused on recruiting more diverse voices and
+                diverse contributors to our staff, our freelance pool, and our
+                Dish Discovery Allstars brand ambassador program. And we are
+                working to ensure that our video and voice programming features
+                the same diversity as our audience.
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        {/* diversity div ends */}
+
+        {/* guidelines div begins */}
+
+        <div>
+          <div>
+            <h2>Community Guidelines</h2>
+          </div>
+
+          <div>
+            <h3>
+              We moderate every review and photograph added to a recipe. When
+              leaving reviews, we ask that you remember that a fellow Dish
+              discovery community member has shared their recipe with you.
+              Constructive feedback is always welcome, but rude, mean,
+              disrespectful, or spammy comments are not welcome and will not be
+              approved.
+            </h3>
+          </div>
+        </div>
+
+        {/* guidelines div ends */}
+
+        {/* product reviews div begins */}
+
+        <div>
+          <div>
+            <h2>product Reviews</h2>
+          </div>
+
+          <div>
+            <h3>
+              Our product reviews are independent and based on research and
+              product testing. If you visit links within our content, we may
+              receive commissions from your purchases, but we never receive any
+              compensation or consideration for the content of our
+              recommendations.
+            </h3>
+          </div>
+        </div>
+
+        {/* product reviews div ends */}
+      </section>
       <Footer />
     </div>
   );
